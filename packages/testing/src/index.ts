131451164a95f8a1ee30a02c982ffa86ae373c2e
Mihai Lupu <mihai.lupu@sensigo.ro>
2026-07-27 17:23:31 +0300

    chore: release v0.33.0

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -64,4 +64,4 @@ export type { TestResult, RunFixtureTestsOptions } from './runner/test-runner.js
 export { startGitHubMockServer } from './servers/github-mock-server.js';
 export type { GitHubMockServerHandle } from './servers/github-mock-server.js';
 
-export const VERSION = '0.32.0';
+export const VERSION = '0.33.0';
